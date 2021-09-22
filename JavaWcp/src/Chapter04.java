3554808535d5e8c15147b794acb3171b178ab371
sayapanman <sayapanman@localhost>
2021-09-22 17:31:11 +0900

    Start chapter 3-4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name = "榎並清香";
+		System.out.println(name);
+
+		name = "へけ";
+		System.out.println(name);
+
+		final int NUM = 7;
+		System.out.println(NUM);
+	}
+
+}
