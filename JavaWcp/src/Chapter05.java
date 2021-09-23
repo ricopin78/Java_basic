574ed9939547df26475c305d1e875e403677d431
sayapanman <sayapanman@192.168.32.154>
2021-09-23 18:10:26 +0900

    Start chapter5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,26 @@
+
+public class Chapter05 {
+	public static void main(String[] args){
+		long long1 = 9223372036854775807L;
+		double double1 = 1234567.89;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		float float1 = 0.123f;
+		int int1 = 78;
+		char char1 = 'ま';
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(char1);
+
+		int [] array1 = new int[5];
+		array1[0] = 1;
+		array1[1] = 2;
+		array1[2] = 3;
+		array1[3] = 4;
+		array1[4] = 5;
+		System.out.println(array1[2]);
+	}
+}
