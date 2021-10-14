b4a56619f3b80babe922b785ad508f3fe8c74a59
sayapanman <sayapanman@localhost>
2021-10-14 21:02:16 +0900

    chapter6

--- JavaWcp/src/Chapter06.java
+++ JavaWcp/src/Chapter06.java
@@ -1,18 +1,19 @@
 
 public class Chapter06 {
 	public static void main(String[] args){
-		long long1 = 9223372036854775807L;
-		double double1 = 1234567.89;
-		boolean boolean1 = true;
-		System.out.println(long1);
-		System.out.println(double1);
-		System.out.println(boolean1);
 
-		float float1 = 0.123f;
-		int int1 = 78;
-		char char1 = 'ま';
-		System.out.println(float1);
-		System.out.println(int1);
-		System.out.println(char1);
+		int val = 2 + 5;
+		System.out.println(val);
+
+		val++;
+	    System.out.println(val);
+
+		System.out.println(val*5);
+		boolean bool = val == 50;
+		System.out.println(bool);
+
+		bool = (val < 50) && (val % 10 == 0);
+		System.out.println(bool);
+
 	}
 }
