aa53f86cbd1967b966873ba89e94eb5397ae18a3
sayapanman <sayapanman@localhost>
2021-10-20 20:17:42 +0900

    chapter7

---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,15 @@
+
+public class Chapter07 {
+    public static void main(String[] args) {
+        int number = 12;
+        if (number <= 5) {
+            System.out.println("とても近いです");
+        } else if (number <= 10) {
+            System.out.println("近いです");
+        } else if (number <= 15) {
+            System.out.println("遠いです");
+        } else {
+            System.out.println("とても遠いです");
+        }
+    }
+}
